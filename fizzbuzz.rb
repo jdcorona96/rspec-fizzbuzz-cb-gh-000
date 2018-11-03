829e5418055283cd7656410370d706ceac28b1d3
Joseph Corona <josed.hc96@gmail.com>
2018-11-03 15:30:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz (in)
+
+  if in % 3 == 0 && in % 5 == 0
+    return "FizzBuzz"
+  elsif in % 3 == 0
+     return "Fizz"
+  elsif in % 5 == 0
+    return "Buzz"
+  else
+    return nil
+  end
+  
+
+  
+    
+    
